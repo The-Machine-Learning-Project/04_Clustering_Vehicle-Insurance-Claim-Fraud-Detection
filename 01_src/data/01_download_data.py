dac2b77b4c0240adf2e769f7a7d837d086247140
Lumine_ <70693859+InsightInspector@users.noreply.github.com>
2024-03-06 18:35:49 +0800

    Updated code to automatically download the dataset from kaggle

--- 01_src/data/01_download_data.py
+++ 01_src/data/01_download_data.py
@@ -1,25 +1,44 @@
 # Download data from Kaggle
 
-# TODO: STUDY HOW TO DOWNLOAD FILES FROM KAGGLE USING THE API -- https://www.kaggle.com/datasets/shivamb/vehicle-claim-fraud-detection
+""" SETUP REQUIREMENTS:
+    1. Download API Key (kaggle.json) from kaggle
+    2. Move the kaggle.json file to the same directory as this code
+"""
 
-# import subprocess
+import opendatasets
+import os
+import shutil
 
-# def download_kaggle_dataset(username, dataset_name, destination_path='.'):
-#     # Replace 'username/dataset-name' with the actual username and dataset name
-#     dataset_full_name = f'{username}/{dataset_name}'
+def download_and_move_kaggle_dataset(dataset_full_path, source_folder, destination_folder):
+    # Download the dataset to the specified destination folder
+    opendatasets.download(dataset_full_path)
 
-#     # Download the dataset to the specified destination path
-#     download_command = f'kaggle datasets download {dataset_full_name} -p {destination_path}'
-#     subprocess.run(download_command, shell=True)
+    # Move files to the specified folder
+    move_files(source_folder, destination_folder)
 
-# if __name__ == "__main__":
-#     # Replace 'your_username' and 'your_dataset_name' with your Kaggle username and dataset name
-#     kaggle_username = 'shivamb'
-#     kaggle_dataset_name = 'vehicle-claim-fraud-detection'
+def move_files(source_folder, destination_folder):
+    # Iterate through files in the source folder
+    for filename in os.listdir(source_folder):
+        source_path = os.path.join(source_folder, filename)
+        destination_path = os.path.join(destination_folder, filename)
 
-#     # Specify the destination path (default is the current directory)
-#     destination_path = '././02_data/01_raw/vehicle_insurance_data.csv'
+        # Move the file to the destination folder
+        shutil.move(source_path, destination_path)
 
-#     # Call the function to download the dataset
-#     download_kaggle_dataset(kaggle_username, kaggle_dataset_name, destination_path)
+        # Delete the source folder
+        os.rmdir(source_folder)
 
+if __name__ == "__main__":
+    # Replace 'your_username' and 'your_dataset_name' with your Kaggle username and dataset name
+    dataset_full_path = 'https://www.kaggle.com/datasets/shivamb/vehicle-claim-fraud-detection'
+
+    # Specify the folder for downloading
+    source_folder = dataset_full_path.split('/')[-1]
+
+    # Specify the folder where you want to move the files
+    destination_folder = '../../02_data/01_raw/'
+
+    # Call the function to download and move the dataset
+    download_and_move_kaggle_dataset(dataset_full_path, source_folder, destination_folder)
+    
+    print(f"Downloaded file successfully to {destination_folder}!")
