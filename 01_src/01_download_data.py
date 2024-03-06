997c243779316c706e18c36eb42ee02f6e829438
Lumine_ <70693859+InsightInspector@users.noreply.github.com>
2024-03-06 11:17:13 +0800

    Add codes to download raw data

--- 01_src/01_download_data.py
+++ 01_src/01_download_data.py
@@ -1,2 +1,25 @@
+# Download data from Kaggle
+
+
+# Import Libraries
 import pandas as pd
+import requests
+
+
+# URL to the file
+path = 'https://www.kaggle.com/datasets/shivamb/vehicle-claim-fraud-detection/download?datasetVersionNumber=1'
+
+# Send a GET request to the URL
+response = requests.get(path)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+
+    # Save the content to a local CSV file
+    with open('./02_data/01_raw/vehicle_insurance_data.csv', 'wb') as local_file:
+        local_file.write(response.content)
+    print('File downloaded successfully.')
+    
+else:
+    print(f'Failed to download the file. Status code: {response.status_code}')
 
